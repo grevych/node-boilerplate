172cb21a8684e891644e75d52c3736f3a8fb3bfd
Gerardo Reyes <grevych@gmail.com>
2019-05-23 19:55:24 -0500

    Fix database module:  - Add mongoose database  - Fix database factory  - Clean database errors

--- index.js
+++ index.js
@@ -4,10 +4,8 @@ const config = require('config');
 const api = require('./api/v1');
 const databaseFactory = require('./lib/database/factory');
 
-
 const db = databaseFactory.create(config.database);
 const app = api(db);
 
-app.disable('x-powered-by')
 app.listen(config.api.port)
 console.log("Marketplace listening on ", config.api.port);
